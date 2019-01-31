17f706cab7f5add2a43db2e87627c0c89b02faf3
Kevin Yu <learn@learn15.local>
2019-01-31 15:10:59 -0800

    first commit

---
+++ src/pages/Cats.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import {
+  Container,
+  Col,
+  Row,
+  ListGroup,
+  ListGroupItem
+} from 'react-bootstrap'
+import '../App.css';
+
+class Cats extends Component {
+  render() {
+    return(
+      <Container>
+      <Row>
+        <Col xs={12}>
+          <ListGroup>
+          {this.props.cats.map((cat,index)=>{
+            return (
+              <div key={index}>
+                  <h4 >
+                  <span className='cat-name'>
+                  {cat.name}
+                  </span>
+                  - <small className='cat-age'>{cat.age} years old</small>
+                  </h4>
+                <span className='cat-enjoys'>
+                  {cat.enjoys}
+                  </span>
+                </div>
+            )
+          })}
+          </ListGroup>
+        </Col>
+      </Row>
+      </Container>
+
+    )
+  }
+}
+
+export default Cats;
